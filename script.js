5f0fca646b71dc569904cbec6318e6eb7660d6fb
hasadi-ha <hasanonomy@gmail.com>
2018-12-06 17:53:42 -0500

    created a signup page

--- script.js
+++ script.js
@@ -1,104 +1,106 @@
 var root_url = "http://comp426.cs.unc.edu:3001/"
 
 $(document).ready(() => {
+  let body = $('body');
+  $('#signup_btn').on('click', () => {
+    body.empty();
 
-    $('#signup_btn').on('click', () => {
-        let user = $('#login_user').val();
-        let pass = $('#login_pass').val();
-        console.log(user);
-        console.log(pass);
+    body.append('<div id="mesg_div"></div>');
+    body.append('<div id="login_div"><div>');
+    $('#login_div').append('New Username: <input type="text" id="newlogin_user"><br> New Password: <input type="text" id="newlogin_pass"><br> <button id="newsignup_btn">Sign Up</button>');
 
-        $.ajax(root_url + 'users', {
-            type: 'POST',
-            xhrFields: {withCredentials: true},
-            data: {
-                "user": {
-                    "username": user,
-                    "password": pass
-                }
-            },
-            success: (response) => {
-                alert('success');
-            },
-            error: () => {
-                alert('error');
-            }
-        });
+    let user = $('#newlogin_user').val();
+    let pass = $('#newlogin_pass').val();
+
+    $('#newsignup_btn').on('click', () => {
+      $.ajax(root_url + 'users', {
+        type: 'POST',
+        xhrFields: { withCredentials: true },
+        data: {
+          "username": user,
+          "password": pass
+        },
+        success: (response) => {
+          $('#mesg_div').append('<p>Success</p>')
+        },
+        error: () => {
+          $('#mesg_div').append('<p>Failure</p>')
+        }
+      });
     });
+  });
 
-    $('#login_btn').on('click', () => {
+  $('#login_btn').on('click', () => {
 
-        let user = $('#login_user').val();
-        let pass = $('#login_pass').val();
-        console.log(user);
-        console.log(pass);
+    let user = $('#login_user').val();
+    let pass = $('#login_pass').val();
 
-        $.ajax(root_url + 'sessions', {
-            type: 'POST',
-            xhrFields: {withCredentials: true},
-            data: {
-                "user": {
-                    "username": user,
-                    "password": pass
-                }
-            },
-            success: (response) => {
-                alert('successful login');
-                createMainPage();
-            },
-            error: () => {
-                alert('error login');
-            }
-        });
+    $.ajax(root_url + 'sessions', {
+      type: 'POST',
+      xhrFields: { withCredentials: true },
+      data: {
+        "user": {
+          "username": user,
+          "password": pass
+        }
+      },
+      success: (response) => {
+        alert('successful login');
+        createMainPage();
+      },
+      error: () => {
+        alert('error login');
+      }
     });
+  });
 });
 
 let createMainPage = () => {
-    let body = $('body');
-    body.empty();
-    body.append('<nav class="navbar"><nav>');
-    $('.navbar').append('<button class="home">Home</button');
-    $('.navbar').append('<button class="user">Username</button');
-    $('.navbar').append('<button class="logout">Logout</button');
-    
-    body.append('<div class="search"><div>');
-    $('.search').append('<h2>Destination_Bar_Search</h2>');
-    $('.search').append('Destination: <input type="text" id="location">');
-    $('.search').append('<button id="search_location">Search</button>');
-    
-     $('#search_location').on('click', () => {
-         let location = $('#location').val();
-         console.log(location);        
-     });
-    
-    let airport_list=[];
-    //let location = $('#loction').val();
-    //location=location.toLowerCase();
-    //console.log(location);
-    
-    
-    //gets list of airports
-    $.ajax(root_url + 'airports', {
-        type: 'GET',
-        xhrFields: {withCredentials:true},
-        success: (response) => {
-            let data = response;
-            for (let i=0; i<data.length;i++) {
-                airport_list.push(data[i].city);
-            }
-        }
-    });
-    let clean_airport_list= [];
-    console.log(airport_list);
-    //cleanArray(clean_airport_list);
-    //console.log(clean_airport_list.length);
-    $('#location').autocomplete({source: airport_list});
+  let body = $('body');
+  body.empty();
+  body.append('<nav class="navbar"><nav>');
+  $('.navbar').append('<button class="home">Home</button');
+  $('.navbar').append('<button class="user">Username</button');
+  $('.navbar').append('<button class="logout">Logout</button');
+
+  body.append('<div class="search"><div>');
+  $('.search').append('<h2>Destination_Bar_Search</h2>');
+  $('.search').append('Destination: <input type="text" id="location">');
+  $('.search').append('<button id="search_location">Search</button>');
+
+  $('#search_location').on('click', () => {
+    let location = $('#location').val();
+    console.log(location);
+  });
+
+  let airport_list = [];
+  //let location = $('#loction').val();
+  //location=location.toLowerCase();
+  //console.log(location);
+
+
+  //gets list of airports
+  $.ajax(root_url + 'airports', {
+    type: 'GET',
+    xhrFields: { withCredentials: true },
+    success: (response) => {
+      let data = response;
+      for (let i = 0; i < data.length; i++) {
+        airport_list.push(data[i].city);
+      }
+    }
+  });
+  let clean_airport_list = [];
+  console.log(airport_list);
+  //cleanArray(clean_airport_list);
+  //console.log(clean_airport_list.length);
+  $('#location').autocomplete({ source: airport_list });
 }
 
 
 let cleanArray = (a) => {
-    var seen= {};
-    return a.filter(function(item) {
-        return seen.hasOwnProperty(item) ? false : (seem[item]=true);
-    });
+  var seen = {};
+  return a.filter(function (item) {
+    return seen.hasOwnProperty(item) ? false : (seem[item] = true);
+  });
 }
